b5857ee63ecc09bba0a607091108bfbda6cf4824
Yining Han <yinhan@Yinings-MBP.wv.cc.cmu.edu>
2018-04-18 12:56:57 -0400

    Preliminary PCA and preliminary EDA

--- galaxy.py
+++ galaxy.py
@@ -1,76 +1,84 @@
 import pandas as pd
-import re
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
 import seaborn as sns
 import numpy as np
+import random
+
+
+def invalid_est(df):
+    """
+    a brief function to check the invalid estimates of emission lines
+    output a percentage of missing estimates
+    """
+    print("\n")
+    for column in df.columns:
+        print("{0}:{1}%".format(
+            column, round(100*np.sum(df[column] == 0)/df.shape[0], 2)))
+
+
+def pie_chart(df):
+    idx = random.randint(0, df.shape[0])
+    bool_index = df.loc[idx] != 0
+    label = [column.split("_")[1] for column, ind in
+             zip(df.columns, bool_index) if ind]
+    values = df.loc[idx][bool_index]
+
+    distinct_label = set(label)
+    distinct_value = []
+    for lab in distinct_label:
+        current_value = 0
+        for i, lab2 in enumerate(label):
+            if lab2 == lab:
+                current_value += values[i]
+        distinct_value.append(current_value)
+
+    plt.pie(distinct_value, labels=distinct_label,
+            autopct='%1.1f%%', shadow=True, startangle=140)
+
+    plt.show()
+
 
 data = pd.read_csv("summary_yinhan.csv")
+plt.hist(data["Flux_NI_5200"])
+print(sum(data["Flux_He_3970"] <= 0)/data.shape[0])
+plt.show()
 
-# select all the columns from
-selected_columns = [column for column in data.columns
-                    if not re.match(r'.*[Ee]rr.*', column)]
+data.replace(to_replace=-9999, value=0, inplace=True)
+emission_lines = ['Flux_HeII_3203', 'Flux_NeV_3345', 'Flux_NeV_3425',
+                  'Flux_OII_3726', 'Flux_OII_3728', 'Flux_NeIII_3868',
+                  'Flux_NeIII_3967', 'Flux_H5_3889', 'Flux_He_3970',
+                  'Flux_Hd_4101', 'Flux_Hg_4340', 'Flux_OIII_4363',
+                  'Flux_HeII_4685', 'Flux_ArIV_4711', 'Flux_ArIV_4740',
+                  'Flux_Hb_4861', 'Flux_OIII_4958', 'Flux_OIII_5006',
+                  'Flux_NI_5197', 'Flux_NI_5200', 'Flux_HeI_5875',
+                  'Flux_OI_6300', 'Flux_OI_6363', 'Flux_NII_6547',
+                  'Flux_Ha_6562', 'Flux_NII_6583', 'Flux_SII_6716',
+                  'Flux_SII_6730']
 
-data_train = data[selected_columns]
-data_train = data_train.drop(["specObjID", "ra", "dec"], axis=1)
+emission_data = data[emission_lines]
+pie_chart(emission_data)
+invalid_est(emission_data)
+scaler = preprocessing.StandardScaler().fit(emission_data)
+emission_data_std = scaler.transform(emission_data)
+pca = PCA(n_components=6)
+pca.fit(emission_data_std)
+print(sum(pca.explained_variance_ratio_))
 
-quantiles = [i*0.1 for i in range(11)]
-quantile_z = list(data_train["specz"].quantile(
-    quantiles, interpolation='linear'))
+sns.distplot(emission_data["Flux_NeIII_3967"])
 
-sns.kdeplot(data_train["specz"][data_train['specz'] < 1], shade=True, bw=.5,
-            color="olive", label="redshift")
-for i in range(0, len(quantile_z)-1, 2):
-    plt.axvline(x=quantile_z[i])
+filter_quantile = np.all([emission_data[col] < emission_data[col].quantile(.95)
+                          for col in emission_lines], axis=0)
+filtered_galaxy = emission_data[filter_quantile]
+invalid_est(filtered_galaxy)
+scaler_filtered = preprocessing.StandardScaler().fit(filtered_galaxy)
+filtered_galaxy_std = scaler_filtered.transform(filtered_galaxy)
+pca = PCA(n_components=6)
+pca.fit(filtered_galaxy_std)
+print(sum(pca.explained_variance_ratio_))
+
+sns.distplot(filtered_galaxy["Flux_HeII_4685"])
 plt.show()
 
 
-def bin_data(data_train, quantile):
-    bin_data = []
-    for i in range(len(quantile)-1):
-        bin_data.append(data_train.loc[
-            (quantile[i] <= data_train["specz"]) &
-            (data_train["specz"] < quantile[i+1])])
-    return bin_data
-
-
-data_train_dered = data_train[["objid", "deVRad_u", "deVRad_g", "deVRad_r", "deVRad_i", "deVRad_z", "specz"]]
-bin_dered = bin_data(data_train_dered, quantile_z)
-
-
-def knn_across_bins(data, bin1, bin2):
-    """
-    The data is a list of all the binned numpy array
-    bin1: an int suggest the bin number with smaller specz
-    bin2: an int suggest the bin number with bigger specz
-    return:
-      obj: a list of list given the Sosies' pair of their objid
-    """
-    data_bin1 = data[bin1]
-    data_bin2 = data[bin2]
-    closest = []
-
-    for i in range(data_bin1.shape[0]):
-        distance = 0
-        for feature in ["deVRad_u",  "deVRad_g",  "deVRad_r",  "deVRad_i",  "deVRad_z"]:
-            distance += abs(data_bin2[feature]
-                            - data_bin1[feature].iloc[i])
-        closest.append(data_bin2["objid"].loc[distance.idxmin()])
-    obj = [(row[0], row[1]) for
-           row in zip(*[list(data_bin1["objid"]), closest])]
-    return obj
-
-
-lst = knn_across_bins(bin_dered, 1, 4)
-lst2 = knn_across_bins(bin_dered, 1, 7)
-
-x = np.linspace(1, 4, 100)
-y = x**(3/2)
-plt.plot(x, y)
-plt.xlabel("Distance Ratio(apparent luminosity)")
-plt.ylabel("Velocity(redshift)")
-plt.title("Objective")
-plt.axvline(x=1.2, ymax=1.2**(3/2)/4**(3/2), color="b")
-plt.axvline(x=2, ymax=2**(3/2)/4**(3/2), color="b")
-plt.axvline(x=3, ymax=3**(3/2)/4**(3/2), color="b")
-plt.axvline(x=3.5, ymax=3.5**(3/2), color="b")
-plt.show()
