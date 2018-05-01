bb8b1b194f90f3528ec307d075710fe61d334900
Eva Gjekmarkaj <gjekmarkaje@gmail.com>
2018-05-01 00:19:50 -0400

    New Changes

--- Galaxy_Project.R
+++ Galaxy_Project.R
@@ -1,81 +1,46 @@
-#Code so far 
-
 #Reading in the Data in R
+library(data.table)
 system.time(newData <- fread("galaxie_data.csv", header = TRUE))
 newData <- replace(newData, newData == "-9999", NA)
 newData <- data.frame(newData)
 
-#Removes column if more than 25% of 0's
+
+data2Bins <- .bincode(newData$specz, breaks = quantile(newData$specz, seq(0,1, by = 0.1)), 
+                      include.lowest = TRUE)
+newData <- cbind(newData, data2Bins)
+head(newData)
+str(newData)
+
+#Removes column if more than 25% of 0's from each unique bin
+
 deleteCol <- c()
-for(i in 1:ncol(newData)) {
-  if(length(which(newData[,i] == 0)) > 0.25*nrow(newData)) {
-    deleteCol <- append(deleteCol,i) 
+for(i in 1:unique(newData$data2Bins)) {
+  for(j in 1:ncol(newData)) {
+    if(length(which(newData[,j] == 0)) > 0.25*nrow(newData)) {
+      deleteCol <- append(deleteCol,j)
+    }
   }
 }
 data2 <- newData[,-deleteCol]
+
+#Separating Flux Data and Photometric Data
+fluxData <- data2[,c(2,4,53:63)]
+photoData <- data2[,c(2,63, 8:52)]
 
 #Quick summary of data2
 head(data2)
 str(data2)
 
-#Binning redshifts in 10 bins
-data2Bins <- .bincode(data2$specz, breaks = quantile(data2$specz, seq(0,1, by = 0.1)), 
-                      include.lowest = TRUE)
-data3 <- cbind(data2, data2Bins)
-head(data3)
+#PCA on Photometric Data by bin that spits out 
 
-#PCA in flux data (not normalized)
-fluxData <- data3[,53:63]
-fluxPCA <- prcomp(na.omit(fluxData), center = TRUE, scale = TRUE)
-print(fluxPCA)
-plot(fluxPCA, type = "l")
-summary(fluxPCA)
-
-#PCA on photometric data
-library(dplyr)
-library(ggfortify)
-photoData <- data3[,c(63,8:52)]
-
-#PCA on bin 1
-bin1 <- filter(photoData, photoData$data2Bins == 1)
-bin1PCA <- prcomp(na.omit(bin1[,2:45]), center = TRUE, scale = TRUE)
-print(bin1PCA)
-#plot(bin1PCA, type = "l")
-summary(bin1PCA)
-autoplot(bin1PCA, loadings = TRUE)
-
-#PCA on bin 2
-bin2 <- filter(photoData, photoData$data2Bins == 2)
-bin2PCA <- prcomp(na.omit(bin2[,2:45]), center = TRUE, scale = TRUE)
-print(bin2PCA)
-#plot(bin1PCA, type = "l")
-summary(bin2PCA)
-autoplot(bin2PCA, loadings = TRUE)
-
-#PCA on bin 3
-bin3 <- filter(photoData, photoData$data2Bins == 3)
-bin3PCA <- prcomp(na.omit(bin3[,2:45]), center = TRUE, scale = TRUE)
-print(bin3PCA)
-#plot(bin1PCA, type = "l")
-summary(bin3PCA)
-autoplot(bin3PCA, loadings = TRUE)
-
-#PCA on bin 4
-bin4 <- filter(photoData, photoData$data2Bins == 4)
-bin4PCA <- prcomp(na.omit(bin4[,2:45]), center = TRUE, scale = TRUE)
-print(bin4PCA)
-#plot(bin1PCA, type = "l")
-summary(bin4PCA)
-autoplot(bin4PCA, loadings = TRUE)
-
-
-#PCA on bin 5
-bin5 <- filter(photoData, photoData$data2Bins == 5)
-bin5PCA <- prcomp(na.omit(bin5[,2:45]), center = TRUE, scale = TRUE)
-print(bin5PCA)
-#plot(bin1PCA, type = "l")
-summary(bin5PCA)
-autoplot(bin5PCA, loadings = TRUE)
+lapply(photoData[,c(3:47)], FUN = class)
+par(mfrow = c(2,2), mar=c(4,4,2,1))
+by(photoData, INDICES = photoData$data2Bins, FUN = function(z) {
+  pca <- prcomp(z[,unlist(lapply(z, FUN = class)) == "numeric"], scale = TRUE, center = TRUE)
+  #plot(pca$x[,1:2], pch=16, main=z[1,"Photometric"])
+  summary(pca)
+  z
+})
 
 
 
